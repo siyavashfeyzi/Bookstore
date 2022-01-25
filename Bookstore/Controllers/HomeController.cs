5251fe1976ee96d4b0dab4f3e8a58d174568f5ff
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-25 15:35:34 +0330

    Add Support Page to website

--- Bookstore/Controllers/HomeController.cs
+++ Bookstore/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace Bookstore.Controllers
             return View();
         }
 
+        public IActionResult Support()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
