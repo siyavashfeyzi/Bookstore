3c011b1e7d705f854e85c632fff811d6087b3a9b
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-25 19:15:25 +0330

    Add Category table

--- Bookstore.DataAccess/Data/ApplicationDbContext.cs
+++ Bookstore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace Bookstore.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
