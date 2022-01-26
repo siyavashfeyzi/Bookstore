d4306ee74d7eaadea7127cc08aa881a7e1e9a57e
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-26 22:38:27 +0330

    Add Product Controller Part 2

--- Bookstore/Areas/Admin/Controllers/ProductController.cs
+++ Bookstore/Areas/Admin/Controllers/ProductController.cs
@@ -1,11 +1,13 @@
 ﻿using Bookstore.DataAccess.Repository.IRepository;
 using Bookstore.Models;
+using Bookstore.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bookstore.Areas.Admin.Controllers
 {
@@ -29,51 +31,63 @@ namespace Bookstore.Areas.Admin.Controllers
 
         public IActionResult Upsert(int? id)
         {
-            var product = new Product();
-
+            ProductVM productVM = new ProductVM() 
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i=> new SelectListItem 
+                { 
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
             if (id == null)
             {
                 //this is for create 
-                return View(product);
+                return View(productVM);
             }
 
             // this is for edit
-            product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
 
-            if (product == null)
+            if (productVM.Product == null)
             {
                 return NotFound();
             }
-            return View(product);
+            return View(productVM);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                if (product.Id == 0)
-                {
-                    _unitOfWork.Product.Add(product);
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Upsert(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        if (product.Id == 0)
+        //        {
+        //            _unitOfWork.Product.Add(product);
 
-                }
-                else
-                {
-                    _unitOfWork.Product.Update(product);
-                }
-                _unitOfWork.Save();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
+        //        }
+        //        else
+        //        {
+        //            _unitOfWork.Product.Update(product);
+        //        }
+        //        _unitOfWork.Save();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(product);
+        //}
 
         #region API CALLS
 
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Product.GetAll();
+            var allObj = _unitOfWork.Product.GetAll(includeProperties : "Category,CoverType");
             return Json(new { data = allObj });
         }
 
