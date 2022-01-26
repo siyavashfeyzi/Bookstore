0251868781361450a70674bff6bb1ff30cf13353
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-26 14:30:56 +0330

    Add Category Index View

---
+++ Bookstore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using Bookstore.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        /*
+           [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allObj = await _unitOfWork.Category.GetAllAsync();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var objFromDb = await _unitOfWork.Category.GetAsync(id);
+            if (objFromDb == null)
+            {
+                TempData["Error"] = "Error deleting Category";
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            await _unitOfWork.Category.RemoveAsync(objFromDb);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Category successfully deleted";
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+        */
+        #endregion
+
+    }
+}
