d19c73baa9e7b853b29775b301c0be091b2cf39f
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-26 22:11:13 +0330

    Add Product Controller

---
+++ Bookstore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,127 @@
+﻿using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            var product = new Product();
+
+            if (id == null)
+            {
+                //this is for create 
+                return View(product);
+            }
+
+            // this is for edit
+            product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(product);
+
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(product);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                TempData["Error"] = "خطا در حذف دسته";
+                return Json(new { success = false, message = "خطای حذف" });
+            }
+
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "دسته با موفقیت حذف شد";
+            return Json(new { success = true, message = "حذف با موفقیت انجام شد" });
+
+        }
+
+        /*
+           [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allObj = await _unitOfWork.Product.GetAllAsync();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var objFromDb = await _unitOfWork.Product.GetAsync(id);
+            if (objFromDb == null)
+            {
+                TempData["Error"] = "Error deleting Product";
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            await _unitOfWork.Product.RemoveAsync(objFromDb);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Product successfully deleted";
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+        */
+        #endregion
+
+    }
+}
