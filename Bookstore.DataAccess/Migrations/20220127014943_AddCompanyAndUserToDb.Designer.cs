ea306059ab9424718d4b5d2ccae9d34e25876ef9
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-27 05:20:48 +0330

    Solution - Company and Application User Models

--- Bookstore.DataAccess/Migrations/20220127014943_AddCompanyAndUserToDb.Designer.cs
+++ Bookstore.DataAccess/Migrations/20220127014943_AddCompanyAndUserToDb.Designer.cs
@@ -4,14 +4,16 @@ using Bookstore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bookstore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220127014943_AddCompanyAndUserToDb")]
+    partial class AddCompanyAndUserToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Bookstore.Models/Company.cs
+++ Bookstore.Models/Company.cs
@@ -1,29 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bookstore.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class Company
     {
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string PostalCode { get; set; }
-
-        public int? CompanyId { get; set; }
-
-        [ForeignKey("CompanyId")]
-        public Company Company { get; set; }
-
-        [NotMapped]
-        public string Role { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsAuthorizedCompany { get; set; }
     }
 }
