e63e2c3db01415b77ee82725e79035da60cc73d1
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2022-01-27 06:10:12 +0330

    Company and User

---
+++ Bookstore.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bookstore.DataAccess.Data;
+using Bookstore.DataAccess.Repository.IRepository;
+using Bookstore.Models;
+using System.Linq;
+
+namespace Bookstore.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+    }
+}
--- Bookstore.DataAccess/Repository/UnitOfWork.cs
+++ Bookstore.DataAccess/Repository/UnitOfWork.cs
@@ -19,6 +19,7 @@ namespace Bookstore.DataAccess.Repository
             CoverType = new CoverTypeRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
             SP_Call = new SP_Call(_db);
         }
 
@@ -29,6 +30,8 @@ namespace Bookstore.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
 
         public IProductRepository Product { get; private set; }
+
+        public IApplicationUserRepository ApplicationUser { get; private set; }
 
         public ISP_Call SP_Call{ get; private set; }
 
